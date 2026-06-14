4bd080c4f05c2cf978d3949a768be97be2be8cd7
Hassan Mahmoud <officialhassansmail@gmail.com>
2026-06-14 12:07:55 +0300

    - starting hw4: badger book (React!)

---
+++ HWs/hw4/src/components/Student.jsx
@@ -0,0 +1,13 @@
+const Student = (props) => {
+  return (
+    <div>
+      <h2>
+        {props.name.first} {props.name.last}
+      </h2>
+      {/* TODO Student data goes here! */}
+    </div>
+  );
+};
+
+export default Student;
+
