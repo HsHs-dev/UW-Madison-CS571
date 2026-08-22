658e1e198db305c2b9528ae57dbf80d8ae2bea2f
Hassan Mahmoud <officialhassansmail@gmail.com>
2026-08-22 12:05:52 +0300

    - solve hw6 task 1 and 2: diplay chatrooms and and display badger   messsages

---
+++ HWs/hw6/src/components/content/BadgerChatroom.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import BadgerMessage from "./BadgerMessage.jsx";
+
+export default function BadgerChatroom(props) {
+  const [messages, setMessages] = useState([]);
+
+  const loadMessages = () => {
+    fetch(
+      `https://cs571.org/rest/s25/hw6/messages?chatroom=${props.name}&page=1`,
+      {
+        headers: {
+          "X-CS571-ID": CS571.getBadgerId(),
+        },
+      },
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setMessages(json.messages);
+      });
+  };
+
+  // Why can't we just say []?
+  // The BadgerChatroom doesn't unload/reload when switching
+  // chatrooms, only its props change! Try it yourself.
+  useEffect(loadMessages, [props]);
+
+  return (
+    <>
+      <h1>{props.name} Chatroom</h1>
+      {/* TODO: Allow an authenticated user to create a post. */}
+      <hr />
+      {messages.length > 0 ? (
+        <>
+          <Container fluid>
+            <Row>
+              {messages.map((msg) => {
+                return (
+                  <Col xs={12} md={6} lg={4} xxl={3} key={msg.created}>
+                    <BadgerMessage
+                      created={msg.created}
+                      title={msg.title}
+                      poster={msg.poster}
+                      content={msg.content}
+                    />
+                  </Col>
+                );
+              })}
+            </Row>
+          </Container>
+        </>
+      ) : (
+        <>
+          <p>There are no messages on this page yet!</p>
+        </>
+      )}
+    </>
+  );
+}
